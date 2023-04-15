c1a046d444d7893884e60dcd2685990014afe0af
kangjian.wu@xuanyutech.net <kangjian.wu@xuanyutech.net>
2023-04-15 16:14:41 +0800

    230415

---
+++ WPF_test/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_test
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitialSelfComponent();
+            textbox1.TextChanged += Textbox1_TextChanged;
+        }
+
+        private void InitialSelfComponent()
+        {
+            TestOption newbtn = new TestOption();
+            this.stack_button.Children.Add(newbtn);
+        }
+
+        private void Textbox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            label1.Content = textbox1.Text;
+        }
+    }
+}
